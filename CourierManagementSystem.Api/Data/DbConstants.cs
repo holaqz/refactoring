0d6b29f43ef26ce25f23835a3acb8d00c9483fdf
holaqz <arturhajtbaev2260@gmail.com>
2025-11-27 14:40:25 +0500

    забыл

---
+++ CourierManagementSystem.Api/Data/DbConstants.cs
@@ -0,0 +1,22 @@
+namespace CourierManagementSystem.Api.Data.Configurations
+{
+    public static class DbConstatnts
+    {
+        public const int latitude_precision = 10;
+        public const int Latitude_scale = 8;
+        public const int longitude_precision = 11;
+        public const int longitude_scale = 8;
+        public const int weight_precision = 8;
+        public const int weight_scale = 2;
+        public const int length_precision = 6;
+        public const int length_scale = 2;
+        public const int width_precision = 6;
+        public const int width_scale = 2;
+        public const int height_precision = 6;
+        public const int height_scale = 2;
+        public const int maxweight_precision = 8;
+        public const int maxweight_scale = 2;
+        public const int maxvolume_precision = 8;
+        public const int maxvolume_scale = 3;
+    }
+}
